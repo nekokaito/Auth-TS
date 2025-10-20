41a69a6b7bfe9d7805191137a3d52a32122ced83
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2025-10-20 20:54:05 +0600

    Auth Actions: Sign Up, Sign In, SignOut

---
+++ lib/actions/auth-actions.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { headers } from "next/headers";
+import { auth } from "../auth";
+
+export const signUp = async (email: string, password: string, name: string) => {
+  const result = await auth.api.signUpEmail({
+    body: {
+      email,
+      password,
+      name,
+      callbackURL: "/dashboard",
+    },
+  });
+
+  return result;
+};
+
+export const signIn = async (email: string, password: string) => {
+  const result = await auth.api.signInEmail({
+    body: {
+      email,
+      password,
+      callbackURL: "/dashboard",
+    },
+  });
+
+  return result;
+};
+
+export const signOut = async () => {
+  const result = await auth.api.signOut({ headers: await headers() });
+
+  return result;
+};
